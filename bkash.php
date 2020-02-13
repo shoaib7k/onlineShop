e3e0382bd033387fed4e1e594413e7db6b4210ca
shoaib7k <shoaib7k@live.com>
2020-02-13 13:01:29 +0100

    Add files via upload

---
+++ bkash.php
@@ -0,0 +1,11 @@
+<?php include "header1.php"?>
+<form action="insert.php" method="POST">
+Name &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="name"><br>
+
+Mobile Number <input type="text" name="number"><br>
+Address of your area<br><textarea name="address" rows="2" cols="40"></textarea><br>
+<input type="submit">
+
+
+
+</form>
